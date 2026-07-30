6d80ba036ba00a1523496d2e13664b8f14a8cd8e
atomnoid <aayushs2580@gmail.com>
2026-07-31 04:18:03 +0530

    removed an extra div

--- chapterhq/src/components/layouts/dashboard-shell.tsx
+++ chapterhq/src/components/layouts/dashboard-shell.tsx
@@ -215,13 +215,14 @@ export function DashboardShell({ children, user }: DashboardShellProps) {
 									) : null}
 								</div>
 							</div>
-						</header>
+                        </div>
+					</header>
 
 						<div className="flex-1 px-4 py-6 sm:px-6 lg:px-8">
 							<main className="mx-auto w-full max-w-7xl">{children}</main>
 						</div>
 					</div>
-				</div>
+				
 
 				<div
 					className={cn(
